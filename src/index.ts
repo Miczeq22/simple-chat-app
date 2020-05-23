a06114d5116a8eb9ea3e081def010d790f5c5651
natus drew <natusdrew@gmail.com>
2020-05-23 18:35:57 -0400

    remove port code

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { Server } from "./server";
 
 const server = new Server();
 
-const PORT : string|number = process.env.PORT || 5000;
-
 server.listen(port => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
